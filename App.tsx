046dae8ac1a8d254c2fa6c1d3ee0a44c013c7a73
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-04 16:42:24 -0300

    feat: create Stack and Tabs navigators for screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,50 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/login/login-screen';
+import SignupScreen from './screens/login/signup-screen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/home/HomeScreen';
+import PostDetailScreen from './screens/post/post-detail-screen';
+import ProfileScreen from './screens/profile/ProfileScreen';
+import ProfilesScreen from './screens/profile/ProfilesScreen';
+import CreatePostsScreen from './screens/post/create-post-screen';
+import { AuthContext, AuthContextProvider } from './context/auth-context';
+import { useContext } from 'react';
+
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
 
 export default function App() {
+  const { token } = useContext(AuthContext)
+  console.log(token)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar />
+      <AuthContextProvider>
+        <NavigationContainer>
+          {!token ? (
+            <Stack.Navigator
+              screenOptions={({ route, navigation }) => ({
+                headerShown: false
+              })}>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Register" component={SignupScreen} />
+            </Stack.Navigator>) : (
+            <Tab.Navigator>
+              <Tab.Screen name='Home' component={HomeScreen} />
+              <Tab.Screen name='Post' component={PostDetailScreen} />
+              <Tab.Screen name='Create Post' component={CreatePostsScreen} />
+              <Tab.Screen name='Profile' component={ProfileScreen} />
+              <Tab.Screen name='Profiles' component={ProfilesScreen} />
+            </Tab.Navigator>)
+          }
+        </NavigationContainer>
+      </AuthContextProvider>
+    </SafeAreaProvider>
   );
 }
 
