7c95ec87bc0fd81e31170bb04e1b874ac257ef1c
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-04 16:44:37 -0300

    feat: create new component to help devel reusable forms

---
+++ components/input/custom-input.tsx
@@ -0,0 +1,21 @@
+import { Input } from "@rneui/base";
+import React from "react";
+import { InputProps } from "../form/auth-form";
+
+type Props = InputProps & {
+  handleChangeState: any
+}
+
+export default function CustomInput({ name, label, handleChangeState }: Props) {
+  return (
+    <>
+      <Input
+        onChangeText={(text) => handleChangeState(name, text)}
+        label={label}
+        secureTextEntry={name.includes('password') ? true : false}
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+    </>
+  )
+}
