5a3bd04e4c404835e7bb49a9a8339bb4b6baa88d
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-04 16:48:19 -0300

    feat: create some empty pages for future development

---
+++ screens/home/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  
+})
---
+++ screens/post/create-post-screen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function CreatePostsScreen() {
+  return (
+    <View>
+      <Text>Create Posts Screen</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  
+})
