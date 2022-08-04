c95246817c325191af9d1c979c64659b6f6be520
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-04 16:46:51 -0300

    feat: ensure create new user on signupscreen

---
+++ screens/login/signup-screen.tsx
@@ -0,0 +1,60 @@
+
+import React, { useContext, useReducer } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, Alert } from "react-native";
+import AuthForm from "../../components/form/auth-form";
+
+import { AuthContext } from "../../context/auth-context";
+export interface SignupProps {
+  username: string
+  email: string,
+  password: string,
+  passwordConfirmation: string
+}
+
+export default function SignupScreen({ navigation }: any) {
+  const { signup } = useContext(AuthContext)
+
+  const handleSubmit = async ({ username, email, password, passwordConfirmation }: SignupProps) => {
+    try {
+      await signup({ username, email, password, passwordConfirmation })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <View style={style.container}>
+      <AuthForm
+        handleSubmit={handleSubmit}
+        title=""
+        formArr={[{
+          label: 'Username',
+          name: 'username'
+        }, {
+          label: 'Email',
+          name: 'email'
+        }, {
+          label: 'Password',
+          name: 'password'
+        }, {
+          label: 'Password Confirmation',
+          name: 'passwordConfirmation'
+        }]
+        }
+        btnText='SignUp'
+        linkText="Already have an Account, Login Here"
+        to='Login'
+        navigation={navigation}
+      />
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 150
+  }
+})
