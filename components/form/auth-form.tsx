9c7c752a3f2c71bcdfdb20a1f9aaffac3dada0ae
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-07 14:38:01 -0300

    refactor: remove title props

--- components/form/auth-form.tsx
+++ components/form/auth-form.tsx
@@ -1,9 +1,10 @@
 import { Input } from "@rneui/base";
-import React, { useReducer } from "react";
+import React, { useContext, useReducer } from "react";
 import { Button, StyleSheet, TouchableOpacity, Text, Image, View } from "react-native";
 import Spacer from "../util/spacer";
 import logo from '../../assets/logo.png'
 import CustomInput from "../input/custom-input";
+import { AuthContext } from "../../context/auth-context";
 
 export interface InputProps {
   name: string
@@ -14,7 +15,6 @@ interface Props {
   handleSubmit: any
   navigation: any
   formArr: InputProps[]
-  title: string,
   btnText: string,
   linkText: string,
   to: string
@@ -24,54 +24,71 @@ interface ActionType {
   type: string,
   payload: any
 }
-interface StateType {
-  [x: string]: string
-}
+
 const reducer = (state: any, actions: ActionType) => {
   switch (actions.type) {
     case "changeState":
       return { ...state, [actions.payload.name]: actions.payload.value }
   }
 }
-export default function AuthForm({ title, formArr, handleSubmit, navigation, btnText, linkText, to }: Props) {
+export default function AuthForm({ formArr, handleSubmit, navigation, btnText, linkText, to }: Props) {
   const initialState = formArr.reduce((acc, value: InputProps) => ({ ...acc, [value.name]: '' }), {})
+
   const [state, dispatch] = useReducer(reducer, initialState)
-  
+
+  const { errMessage, clearErrorMessage } = useContext(AuthContext)
+
   const handleChangeState = (name: string) => {
     return (value: string) => {
       dispatch({ type: 'changeState', payload: { name, value } })
     }
   }
+
   console.log(state)
   return (
     <>
-      <Spacer>
-        <Image source={logo} style={style.image} />
-      </Spacer>
+      <View style={style.container}>
+        <Spacer>
+          <Image source={logo} style={style.image} />
+        </Spacer>
 
-      {formArr.map((inputs, index) => (
-        <CustomInput
-          key={index}
-          name={inputs.name}
-          label={inputs.label}
-          handleChangeState={handleChangeState(inputs.name)}
-        />
-      ))}
+        {formArr.map((inputs, index) => (
+          <CustomInput
+            key={index}
+            name={inputs.name}
+            label={inputs.label}
+            handleChangeState={handleChangeState(inputs.name)}
+          />
+        ))}
+      </View>
       <Button
         title={btnText}
         onPress={() => handleSubmit(state)}
       />
       <Spacer />
       <TouchableOpacity
-        onPress={() => navigation.navigate(to)}>
+        onPress={() => { clearErrorMessage(); navigation.navigate(to) }}>
         <Text style={style.link}>{linkText}</Text>
-
       </TouchableOpacity>
+      { errMessage && (
+        <Spacer>
+          <Text style={style.errorText}>{errMessage}</Text>
+        </Spacer>
+      )}
+
     </>
   )
 }
 
 const style = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  errorText: {
+    textAlign: 'center',
+    color: 'red'
+  },
   image: {
     width: 120,
     height: 120,
