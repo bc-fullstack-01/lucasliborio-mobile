f97ca43fd79dd0311664c96d3d4c9de43ecf4273
lucasliborio <lucasl.troncoso@gmail.com>
2022-08-09 12:20:32 -0300

    refactor: change on Component

--- screens/post/create-post-screen.tsx
+++ screens/post/create-post-screen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function CreatePostsScreen() {
+export default function CreatePostScreen() {
   return (
     <View>
       <Text>Create Posts Screen</Text>
